a21683c621a0230ca364ed855dc5e81ac41b73f5
hekkel <hekkel@6969af58-1542-49b8-b3af-dcaebf5f4018>
2008-12-11 21:25:34 +0000

    removed gst bus code

--- trunk/Sources/MSound.cpp
+++ trunk/Sources/MSound.cpp
@@ -62,14 +62,7 @@ class MAudioSocket
 					MAudioSocket();
 					~MAudioSocket();
 	
-	static gboolean	BusCall(
-						GstBus*		inBus,
-						GstMessage*	inMessage,
-						void*		inData);
-	
 	GstElement*		mPlayer;
-	GstElement*		mSink;
-	GstBus*			mBus;
 };
 
 MAudioSocket::MAudioSocket()
@@ -77,52 +70,22 @@ MAudioSocket::MAudioSocket()
 	gst_init(nil, nil);
 	
 	mPlayer = gst_element_factory_make("playbin", "play");
+	if (mPlayer == nil)
+		THROW(("Failed to create player"));
 	
 	// Instead of using the default audiosink, use the gconfaudiosink, which
 	// will respect the defaults in gstreamer-properties
 
-	g_object_set(G_OBJECT(mPlayer), "audio-sink",
-		gst_element_factory_make("gconfaudiosink", "GconfAudioSink"), nil);
-
-	mBus = gst_pipeline_get_bus(GST_PIPELINE(mPlayer));
-	gst_bus_add_watch(mBus, &MAudioSocket::BusCall, mPlayer);
+	GstElement* sink = gst_element_factory_make("gconfaudiosink", "GconfAudioSink");
+	if (sink == nil)
+		THROW(("Failed to create sink"));
+	
+	g_object_set(G_OBJECT(mPlayer), "audio-sink", sink, nil);
 }
 
 MAudioSocket::~MAudioSocket()
 {
 	g_object_unref(mPlayer);
-	g_object_unref(mBus);
-}
-
-gboolean MAudioSocket::BusCall(
-	GstBus*		inBus,
-	GstMessage*	inMessage,
-	void*		inData)
-{
-	GstElement *play = (GstElement*)inData;
-	GError *err = nil;
-	
-	switch (GST_MESSAGE_TYPE(inMessage))
-	{
-		case GST_MESSAGE_EOS:
-			gst_element_set_state(play, GST_STATE_NULL);
-//			gst_object_unref(GST_OBJECT(play));
-			break;
-		case GST_MESSAGE_ERROR:
-			gst_message_parse_error(inMessage, &err, NULL);
-			PRINT(("gstreamer: %s", err->message));
-			g_error_free(err);
-			break;
-		case GST_MESSAGE_WARNING:
-			gst_message_parse_warning(inMessage, &err, NULL);
-			PRINT(("gstreamer: %s", err->message));
-			g_error_free(err);
-			break;
-		default:
-			break;
-	}
-
-	return true;
 }
 
 MAudioSocket& MAudioSocket::Instance()
